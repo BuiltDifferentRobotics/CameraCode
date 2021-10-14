7b2ff53e874a5bd916c7dee94bdc7c15854aa19c
Arjun Pal <anonamause10@gmail.com>
2021-10-13 19:40:44 -0500

    added shape detection

---
+++ shapeTest.py
@@ -0,0 +1,107 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+cap = cv2.VideoCapture(0)
+
+# Check if the webcam is opened correctly
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+
+while True:
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    lower_red = np.array([0,0,0])
+    upper_red = np.array([255,255,50])
+    
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+    res[res != 0] = 255
+
+    # ksize
+    ksize = (10, 10)
+  
+    # Using cv2.blur() method 
+    res = cv2.blur(res, ksize) 
+
+    img = res
+  
+    # converting image into grayscale image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  
+    # setting threshold of gray image
+    _, threshold = cv2.threshold(gray, 127, 180, cv2.THRESH_BINARY)
+  
+    # using a findContours() function
+    contours, _ = cv2.findContours(
+        threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  
+    i = 0
+  
+    # list for storing names of shapes
+    for contour in contours:
+    
+        # here we are ignoring first counter because 
+        # findcontour function detects whole image as shape
+        if i == 0:
+            i = 1
+            continue
+        
+        # cv2.approxPloyDP() function to approximate the shape
+        approx = cv2.approxPolyDP(
+            contour, 0.01 * cv2.arcLength(contour, True), True)
+
+        # using drawContours() function
+        cv2.drawContours(img, [contour], 0, (0, 0, 255), 5)
+    
+        # finding center point of shape
+        M = cv2.moments(contour)
+        x = 0
+        y = 0
+        if M['m00'] != 0.0:
+            x = int(M['m10']/M['m00'])
+            y = int(M['m01']/M['m00'])
+    
+        # putting shape name at center of each shape
+        if len(approx) == 3:
+            cv2.putText(img, 'Triangle', (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+    
+        elif len(approx) == 4:
+            cv2.putText(img, 'Quadrilateral', (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+    
+        elif len(approx) == 5:
+            cv2.putText(img, 'Pentagon', (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+    
+        elif len(approx) == 6:
+            cv2.putText(img, 'Hexagon', (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+    
+        else:
+            cv2.putText(img, 'circle', (x, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+    
+    # displaying the image after drawing contours
+    cv2.imshow('shapes', img)
+    
+    '''
+    lower_red = np.array([30,150,50])
+    upper_red = np.array([255,255,180])
+    
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('res',res)
+    '''
+    
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+  
